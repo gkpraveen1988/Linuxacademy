14bf6330a50bad8d7f3594ad659946a043045f75
Vassim Shahir <vassim.shahir@above-inc.com>
2017-01-10 21:13:57 +0530

    Fixed py35 compatibility issue

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement, print_function, unicode_literals
 from setuptools import setup
 from linuxacademy_dl import __title__, __version__,\
         __author__, __email__, __license__
@@ -6,7 +7,7 @@ import tempfile
 
 
 try:
-    with tempfile.NamedTemporaryFile('wb+') as rst_file:
+    with tempfile.NamedTemporaryFile('w+') as rst_file:
         subprocess.call(
             [
              "pandoc", "README.md", "-f", "markdown", "-t", "rst",
@@ -16,8 +17,8 @@ try:
         rst_file.seek(0)
         long_description = rst_file.read()
 except OSError:
-    print 'error'
-    long_description = open('README.md').read()
+    print('Pandoc not installed!!')
+    long_description = open('README.md', 'r').read()
 
 
 setup(
@@ -36,7 +37,7 @@ setup(
     packages=['linuxacademy_dl'],
     install_requires=map(
         lambda x: x.strip(),
-        open('requirements.txt').readlines()
+        open('requirements.txt', 'r').readlines()
     ),
     entry_points={
         'console_scripts': [
@@ -44,18 +45,18 @@ setup(
         ]
     },
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Terminals',
-        'Topic :: Utilities',
-        'Topic :: Multimedia :: Video'
+        u'Development Status :: 2 - Pre-Alpha',
+        u'Environment :: Console',
+        u'Intended Audience :: Developers',
+        u'License :: OSI Approved :: BSD License',
+        u'Operating System :: OS Independent',
+        u'Programming Language :: Python',
+        u'Programming Language :: Python :: 2',
+        u'Programming Language :: Python :: 2.7',
+        u'Programming Language :: Python :: 3',
+        u'Programming Language :: Python :: 3.5',
+        u'Topic :: Terminals',
+        u'Topic :: Utilities',
+        u'Topic :: Multimedia :: Video'
     ],
 )
